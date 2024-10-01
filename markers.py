249d198772687286cebb288f442635432b27a9ae
Justin Todd <spinlock.ca@gmail.com>
2024-09-30 17:56:42 -0700

    Add markers

--- markers.py
+++ markers.py
@@ -1,5 +1,7 @@
 import csv
 import json
+import geopy
+import os
 
 # Define the CSV file path
 csv_file = 'markers.csv'
@@ -14,12 +16,24 @@ with open(csv_file, mode='r', newline='', encoding='utf-8') as file:
     # Iterate through each row in the CSV
     for row in reader:
         # Convert the latitude and longitude back to a coordinates list
-        coordinates = [float(row['latitude']), float(row['longitude'])]
+        geolong = float(row['latitude']) if row['latitude'] else None
+        geolat = float(row['longitude']) if row['longitude'] else None
         
+        if not geolat or not geolong:
+            # Use the geopy library to get the coordinates from the address
+            geolocator = geopy.Nominatim(user_agent='markers.py')
+            location = geolocator.geocode(row['address'])
+            if location:
+                geolat = location.latitude
+                geolong = location.longitude
+                print(f"Update coordinates for {row['address']} are {geolat}, {geolong}")
+            else:
+                raise RuntimeError(f"Could not find coordinates for {row['address']}")  
+
         # Create a dictionary for each row and append it to the json_data list
         json_data.append({
             'address': row['address'],
-            'coordinates': coordinates,
+            'coordinates': [geolong, geolat],
             'title': row['title'],
             'summary': row['summary'],
             'icon': row['icon'],
@@ -32,4 +46,5 @@ json_file = 'markers.json'
 with open(json_file, mode='w', encoding='utf-8') as file:
     json.dump(json_data, file, separators=(',', ':'))
 
-print(f"Data successfully written to {json_file}")
+file_size = os.path.getsize(json_file) / 1024 # Get size in bytes
+print(f"{json_file} created! Size: {file_size:.2f} KB")
